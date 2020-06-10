6c3a61dbfb5cf67e8033bc23a6153f58fb54fec6
jasonlu <jason.lu@xc5.io>
2020-06-10 22:01:21 +0800

    Add new source code

--- src/main/java/RBCTest.java
+++ src/main/java/RBCTest.java
@@ -1,12 +1,15 @@
+import java.lang.System;
+
 public class RBCTest {
 
   public String readUserInput() {
     ///... read some thing that is user controllable
-    return "this value only serves to be a place-holder";
+    return "this value only serves to be a place-holder".toString();
   }
 
   public void conductRiskyOperation(String input) {
     // ... do something risky
+    System.out.println(input);
   }
 
   public void test() {
